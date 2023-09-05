a40b550324490a12b42fa2add4881952d6bf39f5
avidcoder123 <umar.tli.edu@gmail.com>
2023-09-05 15:16:39 +0000

    colors

--- src/lib/ownership.ts
+++ src/lib/ownership.ts
@@ -4,10 +4,13 @@ import { map } from "./bootstrap";
 export let OwnershipController = {
     ownershipMap: new Map<string, number>(),
 
+    //Max 8 players
+    colors: ["200ac4", "20ab45", "edb724", "ed8524", "14b8a2", "6e14b8", "b81471", "035e26"],
+
     updateMapColor() {
         let colormap = ""
         OwnershipController.ownershipMap.forEach((owner, country) => {
-            colormap += `"${country}": "#${["200ac4", "20ab45"][owner]}",`
+            colormap += `"${country}": "#${OwnershipController.colors[owner]}",`
         })
 
         ;(map as any).update(JSON.parse(`{${colormap.slice(0, -1)}}`))
