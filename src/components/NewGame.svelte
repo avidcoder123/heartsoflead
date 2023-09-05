825f447706a8abd8129ec4a17806641cfe26063e
avidcoder123 <umar.tli.edu@gmail.com>
2023-09-05 16:50:18 +0000

    finish form

--- src/components/NewGame.svelte
+++ src/components/NewGame.svelte
@@ -1,8 +1,13 @@
+<script>
+    let name = ""
+    let maxPlayers = 2
+</script>
 <div class="flex flex-col p-10 items-center gap-5">
 	<h1 class="text-white text-5xl text-center">Create New Game</h1>
     <h1 class="text-white text-2xl">Game Name</h1>
-    <input class="rounded-md w-96 h-10" />
+    <input bind:value={name} class="rounded-md w-96 h-10" />
     <h1 class="text-white text-2xl">Max players</h1>
-    <input type="range" min="2" max="8" class="w-96 h-6" />
+    <input bind:value={maxPlayers} type="range" min="2" max="8" class="w-96 h-6" />
+    <span class="text-white">{maxPlayers}</span>
     <button class="bg-blue-500 rounded-lg w-52 h-14 ">Create Game</button>
 </div>
