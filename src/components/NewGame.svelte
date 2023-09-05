70338d00aa371cea3978cc9e9112cd8808cdb2f7
avidcoder123 <umar.tli.edu@gmail.com>
2023-09-05 16:47:04 +0000

    new page

---
+++ src/components/NewGame.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col p-10 items-center gap-5">
+	<h1 class="text-white text-5xl text-center">Create New Game</h1>
+    <h1 class="text-white text-2xl">Game Name</h1>
+    <input class="rounded-md w-96 h-10" />
+    <h1 class="text-white text-2xl">Max players</h1>
+    <input type="range" min="2" max="8" class="w-96 h-6" />
+    <button class="bg-blue-500 rounded-lg w-52 h-14 ">Create Game</button>
+</div>
