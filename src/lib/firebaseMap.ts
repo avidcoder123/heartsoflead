813890a686b307614b2b6c0b506a2fcb4d97fbda
avidcoder123 <umar.tli.edu@gmail.com>
2023-09-15 16:20:59 +0000

    save

--- src/lib/firebaseMap.ts
+++ src/lib/firebaseMap.ts
@@ -1,10 +1,15 @@
+import { child, get, ref } from "firebase/database"
+import { db } from "./firebase"
+
 //Add firebase mapping object here
 class FirebaseMap {
     public path = ""
 
     constructor(path: string, gameid: number) {
-        this.path = `/game/${gameid}/data/${path}`
+        this.path = `/game/${gameid}/data/${path}/`
     }
 
-    
+    get(key: string) {
+        get(child(ref(db), this.path + key))
+    }
 }
